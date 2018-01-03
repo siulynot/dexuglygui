4c3a49176db1871066d803e3f8a05b6ebbdca954
satindergrewal <grewal@lavabit.com>
2018-01-04 09:27:18 +1300

    check swaps status and myprice set to timeout at 5 seconds

--- gui/js/simpledexactions.js
+++ gui/js/simpledexactions.js
@@ -3282,6 +3282,7 @@ function check_my_prices(sig){
 	    data: JSON.stringify(ajax_data),
 	    dataType: 'json',
 	    type: 'POST',
+	    timeout: 5000, // sets timeout to 5 seconds
 	    url: url
 	}).done(function(data) {
 		// If successful
@@ -5415,6 +5416,7 @@ function check_swap_status(sig) {
 	    data: JSON.stringify(ajax_data),
 	    dataType: 'json',
 	    type: 'POST',
+	    timeout: 5000, // sets timeout to 5 seconds
 	    url: url
 	}).done(function(data) {
 		// If successful
