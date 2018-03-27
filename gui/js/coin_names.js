a1df55ab5ae462a926d1f70031da258253e37e55
BloodyNora <no@ema.il>
2018-03-28 01:41:58 +0200

    Added missing break;

--- gui/js/coin_names.js
+++ gui/js/coin_names.js
@@ -42,6 +42,7 @@ function return_coin_details(coin) {
 			coin_details.name = 'Dynamic';
 			coin_details.explorer = 'http://dyn.blocksandchain.com/tx/';
 			coin_details.eth = false;
+			break;
 		case 'ROI':
 			coin_details.name = 'ROICoin';
 			coin_details.explorer = 'https://roi-coin-blockexplorer.roi-coin.com/tx/';
