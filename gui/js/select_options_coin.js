52c632ee8d72d130a44072c98f16a2048ea47980
BloodyNora <no@ema.il>
2018-03-29 14:35:12 +0200

    added Sequence/SEQ

--- gui/js/select_options_coin.js
+++ gui/js/select_options_coin.js
@@ -77,6 +77,7 @@ coin_select_options = `<optgroup label="Crypto Coins">
       <option data-content="<img src='img/cryptologo/xre.png' width='30px'/> RevolverCoin (XRE)" data-tokens="RevolverCoin XRE">XRE</option>
       <option data-content="<img src='img/cryptologo/roi.png' width='30px'/> ROIcoin (ROI)" data-tokens="ROICoin ROI">ROI</option>
       <option data-content="<img src='img/cryptologo/scriv.png' width='30px'/> Scriv (SCRIV)" data-tokens="Scriv SCRIV">SCRIV</option>
+      <option data-content="<img src='img/cryptologo/seq.png' width='30px'/> Sequence (SEQ)" data-tokens="Sequence SEQ">SEQ</option>
       <option data-content="<img src='img/cryptologo/sxc.png' width='30px'/> Sexcoin (SXC)" data-tokens="Sexcoin SXC">SXC</option>
       <option data-content="<img src='img/cryptologo/sib.png' width='30px'/> Sibcoin (SIB)" data-tokens="Sibcoin SIB">SIB</option>
       <option data-content="<img src='img/cryptologo/smart.png' width='30px'/> SmartCash (SMART)" data-tokens="SmartCash SMART">SMART</option>
