88374491730ef3a9f8a3fc1051a239b50fdbc4c9
BloodyNora <no@ema.il>
2018-04-04 23:07:57 +0200

    Added Dinero/DIN

--- gui/js/coin_names.js
+++ gui/js/coin_names.js
@@ -38,6 +38,11 @@ function return_coin_details(coin) {
 			coin_details.explorer = 'https://chainz.cryptoid.info/blk//tx.dws?';
 			coin_details.eth = false;
 			break;
+		case 'DIN':
+			coin_details.name = 'Dinero';
+			coin_details.explorer = 'https://explorer.dinerocoin.org/tx/';
+			coin_details.eth = false;
+			break;
 		case 'BUCK':
 			coin_details.name = 'Buck';
 			coin_details.explorer = 'https://explorer.buck.red/tx/';
