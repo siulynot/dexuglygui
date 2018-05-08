42a7a6eb43e5ec37a0472d0302c9593d703375b9
Your Name <lgarciava.fhc@gmail.com>
2018-05-08 16:09:07 -0400

    Version upgrade to 1.0.3-rc

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const electron = require('electron'),
   killmm = require('./ipc/killmm'),
   appBasicInfo = {
     name: 'BarterDEX',
-    version: '1.0.2-rc'
+    version: '1.0.3-rc'
   };
 var shepherd = require('./ipc/shepherd-ipc'),
   MNZdICOIcon;
