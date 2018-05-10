86c5ac2988f7d96305730bdc9d13179d710ea95e
satindergrewal <grewal@lavabit.com>
2018-05-10 16:10:49 +1200

    coinsdb update

--- gui/js/dex_CoinsDB.js
+++ gui/js/dex_CoinsDB.js
@@ -6,3 +6,24 @@ function BarterDEX_Init_CoinsDB() {
 	localStorage.setItem('mm_barterdex_app_info', JSON.stringify(barterDEX_app_info));
 }
 
+function CoinsDB_UpdatedCoinsDbFile() {
+	var update_coinsdb_file = ShepherdIPC({ "command": "coins_db_dl", "data":{"cmd":"update_coins_file"} });
+	console.log(update_coins_file);
+}
+
+
+function CoinsDB_DlIcons(icons_array) {
+	console.log(icons_array); //Expected value is ["kmd","btc","oot"] if multiple. If single ["kmd"]
+	var icons_dl = ShepherdIPC({ "command": "coins_db_dl", "data":{"cmd":"dl_icons","coin_array":icons_array} });
+}
+
+
+function CoinsDB_ReadLocalDB() {
+	//var coinsdbdir = JSON.parse(localStorage.getItem('mm_barterdex_app_info')).CoinsDBDir;
+	//console.log(coinsdbdir);
+
+	var local_coinsdb = ShepherdIPC({ "command": "coins_db_read_db" });
+	//console.log(local_coinsdb)
+	return local_coinsdb;
+}
+
